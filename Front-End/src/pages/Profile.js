a649ee6b869bbb6ce64d3271d264af77d4578862
erickadikah <erickadikah2030@gmail.com>
2023-05-31 03:17:03 +0300

    git add profile implementation fix

--- Front-End/src/pages/Profile.js
+++ Front-End/src/pages/Profile.js
@@ -1,15 +1,12 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import axios from 'axios';
-import { Box, CircularProgress, Card, CardContent, Typography, Avatar, Button } from '@mui/material';
-// import UserPosts from '../components/UserPosts';
+import { Box, CircularProgress} from '@mui/material';
+import { Avatar, Text, Button, Paper } from '@mantine/core';
 import UserPosts from './UserPosts';
 const UserProfile = () => {
-  const [ProfileImage, setProfileImage] = useState({ myFile : "" })
-  const [user, setUser] = useState();
+  const [user, setUser] = useState(null);
   const id = localStorage.getItem('userId');
   const [isLoading, setIsLoading] = useState(false);
-  // const [userAvatar, setUserAvatar] = useState(null);
-  // const [uploadedImageUrl, setUploadedImageUrl] = useState(null);
 
   const sendRequest = useCallback(async () => {
     try {
@@ -37,85 +34,33 @@ const UserProfile = () => {
     );
   }
 
-  // const handleFileUpload = async (e, userId) => {
-  //   e.preventDefault();
-
-  //   // Get the user's selected avatar image file
-  //   const avatarImage = userAvatar;
-
-  //   // Create a new FormData object and append the avatar image file
-  //   // const formData = new FormData();
-  //   // formData.append('avatar', avatarImage);
-  //   // formData.append('id', id);
-
-  //   // Send the POST request to update the user's profile picture
-  //   try {
-  //     setIsLoading(true);
-  //     alert('sending data')
-  //     const res = await axios.post(`http://localhost:5000/api/post/user/avatar`, {
-  //       avatar:avatarImage,
-  //       id:id
-  //     }
- 
-  //     );
-  //     const data = await res.data;
-  //     setIsLoading(false);
-  //     setUploadedImageUrl(data.user.avatarUrl);
-  //     setUser(data.user._id);
-  //   } catch (error) {
-  //     console.log(error);
-  //     setIsLoading(false);
-  //   }
-  // };
-  const url = "https://art-gallery-api-g2ca.onrender.com/uploads"
-
-  const createProfile = async (newImage) => {
-    try {
-        await axios.post(url, newImage)
-    }catch(error) {
-      console.log(error)
-    }
-  }
-
-
-  const handleFileUpload = async (e) => {
-    const file = e.target.files[0];
-    if (file.size > 10 * 1024 * 1024) {
-      console.log("File too large")
-      return
-    }
-    const base64 = await convertToBase64(file)
-    setProfileImage({...ProfileImage, myFile : base64 })
-  }
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    createProfile(ProfileImage)
-    console.log("ProfileImage uploaded")
-  };
-  // const handleFileUpload = async (e) => {
-  //   const file = e.target;
-  // }
-
   return (
     <>
-    <Card>
-      <CardContent>
-        {user ? (
-          <div style={{color: 'black', alignItems: 'center'}}>
-          <Avatar src={ ProfileImage.myFile || user.avatarUrl} sx={{ width: 150, height: 200, margin: '0 auto' }} />
-            <Typography variant="h4" sx={{ textAlign: 'center'}}>{user.name}</Typography>
-            <Typography variant="h6" sx={{ textAlign: 'center'}}>{user.email}</Typography>
-            <input type="file" name="profile" accept="image/*" onChange={ (e) => handleFileUpload(e)} />
-            <Button variant="contained" onClick={handleSubmit}>add Profile Picture</Button>
-          </div>
-        ) : (
-          <Typography variant="body1" sx={{ textAlign: 'center' }}>Loading...</Typography>
+    <div>
+      {user && (
+        <Paper
+          radius="md"
+          withBorder
+          p="lg"
+          sx={(theme) => ({
+            backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.white,
+          })}
+        >
+          <Avatar src='https://images.unsplash.com/photo-1685391317670-8fdb5382a492?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60' size={120} radius={120} mx="auto" />
+          <Text ta="center" fz="lg" weight={500} mt="md">
+            {user.name}
+          </Text>
+          <Text ta="center" c="dimmed" fz="sm">
+            {user.email}
+          </Text>
+
+          <Button variant="default" fullWidth mt="md">
+            Send message
+          </Button>
+        </Paper>
         )}
-      </CardContent>
-    </Card>
-      <div>
-      <h1 style={{color: 'white', textAlign: 'center' }}>My Products</h1>
+        </div>
+        <div>
         <UserPosts />
       </div>
     </>
@@ -123,12 +68,3 @@ const UserProfile = () => {
 };
 
 export default UserProfile;
-
-function convertToBase64(file) {
-  return new Promise((resolve, reject) => {
-    const fileReader = new FileReader();
-    fileReader.readAsDataURL(file);
-    fileReader.onload = () => resolve(fileReader.result.split(',')[1]);
-    fileReader.onerror = (error) => reject(error);
-  });
-}
